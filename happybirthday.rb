a43f8146c97d7dbbbdd5c3b00c21a4587754d978
Allison Rainey <allisonsrainey@gmail.com>
2017-04-23 17:51:55 -0400

    Reordered method

--- happybirthday.rb
+++ happybirthday.rb
@@ -1,22 +1,26 @@
-def calculate_age year, month, day
+def calculate_age
+  puts 'What year were you born?'
+    year = gets.chomp
+
+  puts 'What month were you born?'
+    month = gets.chomp
+
+  puts 'What day were you born?'
+    day = gets.chomp
 
   month_name = ['January', 'February', 'March', 'April',
                 'May', 'June', 'July', 'August',
                 'September', 'October', 'November', 'December']
 
   month_number = month_name.index(month) #+ 1
+
   age = Time.new - Time.local(year, month_number, day)
 
-  puts 'SPANK!' * age
+  return 'SPANK!' * age
 end
 
-puts 'What year were you born?'
-  my_year = gets.chomp
+puts calculate_age
 
-puts 'What month were you born?'
-  my_month = gets.chomp
 
-puts 'What day were you born?'
-  my_day = gets.chomp
 
 calculate_age(my_year, my_month, my_day)
