437122f6f8002b3db7bf0a7ce1c2ef701b78a9a5
Allison Rainey <allisonsrainey@gmail.com>
2017-04-24 20:01:58 -0400

    Initial commit

--- roman-to-integer.rb
+++ roman-to-integer.rb
@@ -1,27 +1,37 @@
-def convert_number roman_numeral
+def convert_number
+  roman_hash = {}
+  roman_hash49 = {}
+
+  roman_hash['I'] = 1
+  roman_hash['V'] = 5
+  roman_hash['X'] = 10
+  roman_hash['L'] = 50
+  roman_hash['C'] = 100
+  roman_hash['D'] = 500
+  roman_hash['M'] = 1000
+
+  roman_hash49['IV'] = 4
+  roman_hash49['IX'] = 9
+  roman_hash49['IL'] = 49
+  roman_hash49['IC'] = 99
+  roman_hash49['ID'] = 499
+  roman_hash49['IM'] = 999
+
   puts 'Please enter a Roman numeral'
     roman_numeral = gets.chomp
 
-  if roman_numeral #need condition to catch invalid characters
-    puts 'That is not a valid Roman numeral.'
-    puts 'Please enter a Roman numeral'
-    roman_numeral = gets.chomp
-  end
+#  if roman_numeral #need condition to catch invalid characters
+#    puts 'That is not a valid Roman numeral.'
+#    puts 'Please enter a Roman numeral'
+#    roman_numeral = gets.chomp
+#  end
 
-  roman_hash = {}
+# start with roman_hash49
+# need to run those over the roman numeral and substitute 'number +'
+# roman_numeral.gsub(/hash-key/, ' number-value +')
 
-  roman_hash['I'] = 1
-  roman_hash['IV'] = 4
-  roman_hash['V'] = 5
-  roman_hash['IX'] = 9
-  roman_hash['X'] = 10
-  roman_hash['IL'] = 49
-  roman_hash['L'] = 50
-  roman_hash['IC'] = 99
-  roman_hash['C'] = 100
-  roman_hash['ID'] = 499
-  roman_hash['D'] = 500
-  roman_hash['IM'] = 999
-  roman_hash['M'] = 1000
 
 end
+
+
+convert_number
